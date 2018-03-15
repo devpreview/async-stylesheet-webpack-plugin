a81074ef2f1b39dc79fddb71ac9e7559b2b43497
Alexey Savchuk <sabo@devpreview.ru>
2018-03-15 11:30:24 +0300

    Plugin implementation

--- src/main/plugin.ts
+++ src/main/plugin.ts
@@ -20,8 +20,50 @@ export class AsyncStylesheetWebpackPlugin extends WebpackPlugin {
 
     public apply(compiler: WebpackCompiler): void {
         compiler.plugin('html-webpack-plugin-alter-asset-tags', (data, next) => {
-            next(null, data);
+            next(null, this.makeStylesheetsAsync(data));
         });
+    }
+
+    public makeStylesheetsAsync(data: any): any {
+        let noscriptTags: string[] = [];
+        for (let tag of data.head) {
+            if (tag.tagName === 'link' && tag.attributes.rel === 'stylesheet') {
+                let noscriptTagAttrs = Object.keys(tag.attributes).map((attr) => {
+                    return attr + '="' + tag.attributes[attr] + '"';
+                }).join(' ');
+                noscriptTags.push('<link ' + noscriptTagAttrs + '>');
+                tag.attributes.rel = 'preload';
+                tag.attributes.as = 'style';
+                tag.attributes.onload = tag.attributes.onload ? tag.attributes.onload : '';
+                tag.attributes.onload += "this.rel='stylesheet';";
+            }
+        }
+        if (noscriptTags.length > 0) {
+            if (this.options.noscriptFallback) {
+                data.head.push({
+                    tagName: 'noscript',
+                    closeTag: true,
+                    innerHTML: noscriptTags.join('')
+                });
+            }
+            /**
+             * rel=preload polyfill
+             * See: https://github.com/filamentgroup/loadCSS#how-to-use-loadcss-recommended-example
+             */
+            if (this.options.preloadPolyfill) {
+                data.head = [{
+                    tagName: 'script',
+                    attributes: {type: 'text/javascript'},
+                    closeTag: true,
+                    innerHTML: fs.readFileSync(
+                        require.resolve('fg-loadcss/dist/cssrelpreload.min.js'),
+                        'utf8'
+                    )
+                }].concat(data.head);
+            }
+        }
+
+        return data;
     }
 
 }
